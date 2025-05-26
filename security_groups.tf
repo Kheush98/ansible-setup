08918ce15a21390e78e6f742602f99e847cdf61f
Cheikh Gaye <cheikhgaye1@esp.sn>
2025-05-26 11:45:09 +0000

    add inbound rule http from everywhere

--- security_groups.tf
+++ security_groups.tf
@@ -52,6 +52,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_controle" {
   to_port                      = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http_from_everywhere" {
+  security_group_id = aws_security_group.client_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
 resource "aws_vpc_security_group_egress_rule" "allo_all_trafic_ipv4_for_client" {
   security_group_id = aws_security_group.client_sg.id
   cidr_ipv4         = "0.0.0.0/0"
